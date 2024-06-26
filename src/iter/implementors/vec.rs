1d245e45dc4d3e90495bb9544ae5edc1c26b8d8e
Ugur Arikan (DHL Data & Analytics) <ugur.arikan@dpdhl.com>
2024-06-26 15:55:19 +0200

    Default constraint is removed from `ConIterOfVec`

--- src/iter/implementors/vec.rs
+++ src/iter/implementors/vec.rs
@@ -6,41 +6,86 @@ use crate::{
     next::NextChunk,
     AtomicCounter, ConcurrentIter, Next,
 };
-use std::{cell::UnsafeCell, cmp::Ordering};
+use std::{
+    cell::UnsafeCell,
+    cmp::Ordering,
+    mem::{ManuallyDrop, MaybeUninit},
+};
 
 /// A concurrent iterator over a vector, consuming the vector and yielding its elements.
 #[derive(Debug)]
-pub struct ConIterOfVec<T: Send + Sync + Default> {
-    vec: UnsafeCell<Vec<T>>,
+pub struct ConIterOfVec<T: Send + Sync> {
+    vec: UnsafeCell<ManuallyDrop<Vec<T>>>,
     vec_len: usize,
     counter: AtomicCounter,
 }
 
-impl<T: Send + Sync + Default> ConIterOfVec<T> {
+impl<T: Send + Sync> Drop for ConIterOfVec<T> {
+    fn drop(&mut self) {
+        let current = self.counter().current();
+        match current.cmp(&self.vec_len) {
+            Ordering::Less => {
+                let _remaining_vec_to_be_dropped = unsafe { self.split_off_right(current) };
+            }
+            _ => {}
+        }
+    }
+}
+
+impl<T: Send + Sync> ConIterOfVec<T> {
     /// Consumes and creates a concurrent iterator of the given `vec`.
     pub fn new(vec: Vec<T>) -> Self {
         Self {
             vec_len: vec.len(),
-            vec: vec.into(),
+            vec: ManuallyDrop::new(vec).into(),
             counter: AtomicCounter::new(),
         }
     }
 
-    #[inline(always)]
-    #[allow(clippy::mut_from_ref)]
-    pub(crate) unsafe fn mut_vec(&self) -> &mut Vec<T> {
-        unsafe { &mut *self.vec.get() }
+    unsafe fn take_one(&self, item_idx: usize) -> T {
+        let vec = &mut *self.vec.get();
+        let src_ptr = vec.as_mut_ptr().add(item_idx);
+
+        let mut value = MaybeUninit::<T>::uninit();
+        let dst_ptr = value.as_mut_ptr();
+
+        dst_ptr.write(src_ptr.read());
+        value.assume_init()
+    }
+
+    pub(crate) unsafe fn take_slice(
+        &self,
+        begin_idx: usize,
+        len: usize,
+    ) -> impl ExactSizeIterator<Item = T> {
+        let vec = &mut *self.vec.get();
+        let end_idx = (begin_idx + len).min(vec.len());
+        let len = end_idx - begin_idx;
+
+        let ptr = vec.as_mut_ptr().add(begin_idx);
+        let vec = Vec::from_raw_parts(ptr, len, 0);
+        vec.into_iter()
+    }
+
+    unsafe fn split_off_right(&self, left_len: usize) -> Vec<T> {
+        debug_assert!(left_len <= self.vec_len);
+
+        let man_vec = &mut *self.vec.get();
+        let mut left_vec: Vec<T> = ManuallyDrop::take(man_vec);
+        let right_vec = left_vec.split_off(left_len);
+        *man_vec = ManuallyDrop::new(left_vec);
+        return right_vec;
     }
 }
 
-impl<T: Send + Sync + Default> From<Vec<T>> for ConIterOfVec<T> {
+impl<T: Send + Sync> From<Vec<T>> for ConIterOfVec<T> {
     /// Consumes and creates a concurrent iterator of the given `vec`.
     fn from(vec: Vec<T>) -> Self {
         Self::new(vec)
     }
 }
 
-impl<T: Send + Sync + Default> AtomicIter<T> for ConIterOfVec<T> {
+impl<T: Send + Sync> AtomicIter<T> for ConIterOfVec<T> {
     #[inline(always)]
     fn counter(&self) -> &AtomicCounter {
         &self.counter
@@ -57,19 +102,13 @@ impl<T: Send + Sync + Default> AtomicIter<T> for ConIterOfVec<T> {
 
     fn get(&self, item_idx: usize) -> Option<T> {
         match item_idx.cmp(&self.vec_len) {
-            Ordering::Less => {
-                // SAFETY: only one thread can access the `item_idx`-th position and `item_idx` is in bounds
-                let vec = unsafe { self.mut_vec() };
-                let value = std::mem::take(&mut vec[item_idx]);
-                Some(value)
-            }
+            // SAFETY: only one thread can access the `item_idx`-th position and `item_idx` is in bounds
+            Ordering::Less => Some(unsafe { self.take_one(item_idx) }),
             _ => None,
         }
     }
 
     fn fetch_n(&self, n: usize) -> Option<NextChunk<T, impl ExactSizeIterator<Item = T>>> {
-        let vec = unsafe { self.mut_vec() };
-
         let begin_idx = self
             .progress_and_get_begin_idx(n)
             .unwrap_or(self.initial_len());
@@ -78,8 +117,7 @@ impl<T: Send + Sync + Default> AtomicIter<T> for ConIterOfVec<T> {
         match begin_idx.cmp(&end_idx) {
             Ordering::Equal => None,
             _ => {
-                let idx_range = begin_idx..end_idx;
-                let values = idx_range.map(|i| unsafe { get_unchecked(vec, i) });
+                let values = unsafe { self.take_slice(begin_idx, n) };
                 Some(NextChunk { begin_idx, values })
             }
         }
@@ -90,30 +128,25 @@ impl<T: Send + Sync + Default> AtomicIter<T> for ConIterOfVec<T> {
     }
 }
 
-impl<T: Send + Sync + Default> AtomicIterWithInitialLen<T> for ConIterOfVec<T> {
+impl<T: Send + Sync> AtomicIterWithInitialLen<T> for ConIterOfVec<T> {
     #[inline(always)]
     fn initial_len(&self) -> usize {
         self.vec_len
     }
 }
 
-#[inline(always)]
-unsafe fn get_unchecked<T: Default>(vec: &mut [T], item_idx: usize) -> T {
-    std::mem::take(&mut vec[item_idx])
-}
+unsafe impl<T: Send + Sync> Sync for ConIterOfVec<T> {}
 
-unsafe impl<T: Send + Sync + Default> Sync for ConIterOfVec<T> {}
-
-unsafe impl<T: Send + Sync + Default> Send for ConIterOfVec<T> {}
+unsafe impl<T: Send + Sync> Send for ConIterOfVec<T> {}
 
 // AtomicIter -> ConcurrentIter
 
-impl<T: Send + Sync + Default> ConcurrentIter for ConIterOfVec<T> {
+impl<T: Send + Sync> ConcurrentIter for ConIterOfVec<T> {
     type Item = T;
 
     type BufferedIter = BufferedVec<T>;
 
-    type SeqIter = std::iter::Skip<std::vec::IntoIter<T>>;
+    type SeqIter = std::vec::IntoIter<T>;
 
     /// Converts the concurrent iterator back to the original wrapped type which is the source of the elements to be iterated.
     /// Already progressed elements are skipped.
@@ -149,8 +182,8 @@ impl<T: Send + Sync + Default> ConcurrentIter for ConIterOfVec<T> {
     /// ```
     fn into_seq_iter(self) -> Self::SeqIter {
         let current = self.counter().current();
-        let vec = self.vec.into_inner();
-        vec.into_iter().skip(current)
+        let remaining_vec = unsafe { self.split_off_right(current.min(self.vec_len)) };
+        remaining_vec.into_iter()
     }
 
     #[inline(always)]
--- tests/consume_vec.rs
+++ tests/consume_vec.rs
@@ -1,90 +1,209 @@
 use orx_concurrent_iter::*;
 use test_case::test_matrix;
 
-const NUM_RERUNS: usize = 1;
+const NUM_RERUNS: usize = 10;
 
-fn concurrent_iter(num_threads: usize, batch: usize, vec: Vec<i64>) {
-    let expected_sum: i64 = vec.iter().sum();
+fn concurrent_iter_stack(num_threads: usize, batch: usize, vec: Vec<i64>, consume_till_end: bool) {
+    let vec_len = vec.len();
+    let expected_sum: Option<i64> = match consume_till_end {
+        true => Some(vec.iter().sum()),
+        false => None,
+    };
     let iter = &vec.into_con_iter();
 
     let sum: i64 = std::thread::scope(|s| {
         let mut handles = vec![];
-        for _ in 0..num_threads {
+        for t in 0..num_threads {
             handles.push(s.spawn(move || {
                 let mut sum = 0i64;
-                if batch == 1 {
-                    while let Some(next) = iter.next_id_and_value() {
-                        sum += next.value;
-                    }
-                } else {
-                    while let Some(chunk) = iter.next_chunk(batch) {
-                        sum += chunk.values.sum::<i64>();
-                    }
+                match consume_till_end {
+                    true => match batch {
+                        1 => {
+                            while let Some(next) = iter.next_id_and_value() {
+                                sum += next.value;
+                            }
+                        }
+                        _ => match t % 2 == 0 {
+                            true => {
+                                while let Some(chunk) = iter.next_chunk(batch) {
+                                    for value in chunk.values {
+                                        sum += value;
+                                    }
+                                }
+                            }
+                            false => {
+                                let mut buffer = iter.buffered_iter(batch);
+                                while let Some(chunk) = buffer.next() {
+                                    for value in chunk.values {
+                                        sum += value;
+                                    }
+                                }
+                            }
+                        },
+                    },
+                    false => match batch {
+                        1 => {
+                            let until = vec_len - 1;
+                            for _ in 0..until {
+                                if let Some(next) = iter.next_id_and_value() {
+                                    sum += next.value;
+                                }
+                            }
+                        }
+                        _ => match t % 2 == 0 {
+                            true => {
+                                let until = vec_len.saturating_sub(batch);
+                                while iter.try_get_len().expect("exists") < until {
+                                    if let Some(chunk) = iter.next_chunk(batch) {
+                                        for value in chunk.values {
+                                            sum += value;
+                                        }
+                                    }
+                                }
+                            }
+                            false => {
+                                let until = vec_len.saturating_sub(batch);
+                                let mut buffer = iter.buffered_iter(batch);
+                                while iter.try_get_len().expect("exists") < until {
+                                    if let Some(chunk) = buffer.next() {
+                                        for value in chunk.values {
+                                            sum += value;
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                    },
                 }
+
                 sum
             }));
         }
 
         handles.into_iter().map(|x| x.join().expect("-")).sum()
     });
-
-    assert_eq!(sum, expected_sum);
-}
-
-#[test_matrix(
-    [1, 4, 1024, 64*1024],
-    [4, 8, 16],
-    [1, 4, 64, 1024]
-)]
-fn consume_vec(len: usize, num_threads: usize, batch: usize) {
-    for _ in 0..NUM_RERUNS {
-        let source: Vec<_> = (0..len).map(|x| x as i64).collect();
-        concurrent_iter(num_threads, batch, source);
+    if let Some(expected_sum) = expected_sum {
+        assert_eq!(sum, expected_sum);
     }
 }
 
-fn concurrent_iter_heap(num_threads: usize, batch: usize, vec: Vec<String>) {
+#[test_matrix(
+    [1, 4, 1024, 2141, 2*1024, 4*1024+1],
+    [4, 8, 16],
+    [1, 4, 64, 1024]
+)]
+fn consume_vec_stack(len: usize, num_threads: usize, batch: usize) {
+    for consume_till_end in [true, false] {
+        for _ in 0..NUM_RERUNS {
+            let source: Vec<_> = (0..len).map(|x| x as i64).collect();
+            concurrent_iter_stack(num_threads, batch, source, consume_till_end);
+        }
+    }
+}
+
+fn concurrent_iter_heap(
+    num_threads: usize,
+    batch: usize,
+    vec: Vec<String>,
+    consume_till_end: bool,
+) {
+    let vec_len = vec.len();
     let iter = &vec.into_con_iter();
 
     let some_str: String = std::thread::scope(|s| {
         let mut handles = vec![];
-        for _ in 0..num_threads {
+        for t in 0..num_threads {
             handles.push(s.spawn(move || {
                 let mut str = String::new();
-                if batch == 1 {
-                    while let Some(next) = iter.next_id_and_value() {
-                        str = next.value;
-                    }
-                } else {
-                    while let Some(chunk) = iter.next_chunk(batch) {
-                        for value in chunk.values {
-                            str = value;
+                match consume_till_end {
+                    true => match batch {
+                        1 => {
+                            while let Some(next) = iter.next_id_and_value() {
+                                str = next.value;
+                            }
                         }
-                    }
+                        _ => match t % 2 == 0 {
+                            true => {
+                                while let Some(chunk) = iter.next_chunk(batch) {
+                                    for value in chunk.values {
+                                        str = value;
+                                    }
+                                }
+                            }
+                            false => {
+                                let mut buffer = iter.buffered_iter(batch);
+                                while let Some(chunk) = buffer.next() {
+                                    for value in chunk.values {
+                                        str = value;
+                                    }
+                                }
+                            }
+                        },
+                    },
+                    false => match batch {
+                        1 => {
+                            let until = vec_len - 1;
+                            for _ in 0..until {
+                                if let Some(next) = iter.next_id_and_value() {
+                                    str = next.value;
+                                }
+                            }
+                        }
+                        _ => match t % 2 == 0 {
+                            true => {
+                                let until = vec_len.saturating_sub(batch);
+                                while iter.try_get_len().expect("exists") < until {
+                                    if let Some(chunk) = iter.next_chunk(batch) {
+                                        for value in chunk.values {
+                                            str = value;
+                                        }
+                                    }
+                                }
+                            }
+                            false => {
+                                let until = vec_len.saturating_sub(batch);
+                                let mut buffer = iter.buffered_iter(batch);
+                                while iter.try_get_len().expect("exists") < until {
+                                    if let Some(chunk) = buffer.next() {
+                                        for value in chunk.values {
+                                            str = value;
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                    },
                 }
                 str
             }));
         }
+
+        let default_str = match consume_till_end {
+            true => String::default(),
+            false => "might-be-empty".to_string(),
+        };
 
         handles
             .into_iter()
             .map(|x| x.join().expect("-"))
             .filter(|x| !x.is_empty())
             .last()
-            .unwrap()
+            .unwrap_or(default_str)
     });
 
     assert!(!some_str.is_empty());
 }
 
 #[test_matrix(
-    [1, 4, 1024, 4*1024],
+    [1, 4, 1024, 2141, 2*1024, 4*1024+1],
     [4, 8, 16],
     [1, 4, 64, 1024]
 )]
 fn consume_vec_heap(len: usize, num_threads: usize, batch: usize) {
-    for _ in 0..NUM_RERUNS {
-        let source: Vec<_> = (0..len).map(|x| x.to_string()).collect();
-        concurrent_iter_heap(num_threads, batch, source);
+    for consume_till_end in [true, false] {
+        for _ in 0..NUM_RERUNS {
+            let source: Vec<_> = (0..len).map(|x| x.to_string()).collect();
+            concurrent_iter_heap(num_threads, batch, source, consume_till_end);
+        }
     }
 }
