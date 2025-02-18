be26aaed9943773b4752fe9f34eeae144777e907
orxfun <orx.ugur.arikan@gmail.com>
2025-02-18 22:16:47 +0100

    IntoConcurrentIter is implemented for &Vec following Iterable pattern

--- src/implementations/slice/mod.rs
+++ src/implementations/slice/mod.rs
@@ -2,5 +2,5 @@
 mod tests;
 
 mod chunks_iter_slice_ref;
-mod con_iter_slice_ref;
+pub(crate) mod con_iter_slice_ref;
 mod slice_into_con_iter;
--- src/implementations/vec/vec_into_con_iter.rs
+++ src/implementations/vec/vec_into_con_iter.rs
@@ -1,5 +1,8 @@
 use super::con_iter_vec::ConIterVec;
-use crate::into_concurrent_iter::IntoConcurrentIter;
+use crate::{
+    implementations::slice::con_iter_slice_ref::ConIterSliceRef,
+    into_concurrent_iter::IntoConcurrentIter,
+};
 use alloc::vec::Vec;
 
 impl<T> IntoConcurrentIter for Vec<T>
@@ -12,5 +15,18 @@ where
 
     fn into_concurrent_iter(self) -> Self::Iter {
         Self::Iter::new(self)
+    }
+}
+
+impl<'a, T> IntoConcurrentIter for &'a Vec<T>
+where
+    T: Send + Sync,
+{
+    type Item = &'a T;
+
+    type Iter = ConIterSliceRef<'a, T>;
+
+    fn into_concurrent_iter(self) -> Self::Iter {
+        Self::Iter::new(self.as_slice())
     }
 }
