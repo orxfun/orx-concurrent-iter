4bd996644d69d56e6c98c57669c2bf3034142eba
orxfun <orx.ugur.arikan@gmail.com>
2025-02-18 06:21:05 +0100

    lifetime ellision

--- src/concurrent_iter.rs
+++ src/concurrent_iter.rs
@@ -33,16 +33,12 @@ pub trait ConcurrentIter<K: NextKind = Regular>: Default {
 
     fn next(&self) -> Option<K::Next<Self::Item>>;
 
-    fn next_chunk<'i>(
-        &'i self,
+    fn chunks_iter(&self, chunk_size: usize) -> Self::ChunkPuller<'_>;
+
+    fn next_chunk(
+        &self,
         chunk_size: usize,
-    ) -> Option<K::NextChunk<Self::Item, <Self::ChunkPuller<'i> as ChunkPuller<K>>::Iter>> {
-        self.in_chunks(chunk_size).pull()
+    ) -> Option<K::NextChunk<Self::Item, <Self::ChunkPuller<'_> as ChunkPuller<K>>::Iter>> {
+        self.chunks_iter(chunk_size).pull()
     }
-
-    fn in_chunks(&self, chunk_size: usize) -> Self::ChunkPuller<'_>;
-
-    // fn chunks_iter(&self, chunk_size: usize) -> ChunksIter<Self::ChunkPuller<'_>, K> {
-    //     ChunksIter::new(self.in_chunks(chunk_size))
-    // }
 }
