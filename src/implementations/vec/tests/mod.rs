04aec0f14a98bea1a91acff24a2f15b56e939da2
orxfun <orx.ugur.arikan@gmail.com>
2025-02-18 22:12:24 +0100

    vec_into_con_iter tests added

--- src/implementations/vec/tests/mod.rs
+++ src/implementations/vec/tests/mod.rs
@@ -1,4 +1,5 @@
 mod con_iter_vec;
 mod con_iter_vec_drop;
 mod seq_chunk_iter_vec;
+mod vec_into_con_iter;
 mod vec_into_seq_iter;
---
+++ src/implementations/vec/tests/vec_into_con_iter.rs
@@ -0,0 +1,31 @@
+use crate::{concurrent_iter::ConcurrentIter, IntoConcurrentIter};
+use orx_concurrent_bag::ConcurrentBag;
+
+#[test]
+fn vec_into_con_iter() {
+    let (nt, n) = (2, 177);
+    let vec: Vec<_> = (0..n).map(|x| (x + 10).to_string()).collect();
+    let iter = vec.into_concurrent_iter();
+
+    let bag = ConcurrentBag::new();
+    let num_spawned = ConcurrentBag::new();
+    std::thread::scope(|s| {
+        for _ in 0..nt {
+            s.spawn(|| {
+                num_spawned.push(true);
+                while num_spawned.len() < nt {} // allow all threads to be spawned
+
+                while let Some(x) = iter.next() {
+                    bag.push(x);
+                }
+            });
+        }
+    });
+
+    let mut expected: Vec<_> = (0..n).map(|i| (i + 10).to_string()).collect();
+    expected.sort();
+    let mut collected = bag.into_inner().to_vec();
+    collected.sort();
+
+    assert_eq!(expected, collected);
+}
