1bf7dec04cfa9e5edab7c1972ce96b9c6407b17c
orxfun <orx.ugur.arikan@gmail.com>
2025-02-18 16:05:49 +0100

    vec is destroyed on construction

--- src/implementations/vec/con_iter_vec.rs
+++ src/implementations/vec/con_iter_vec.rs
@@ -6,6 +6,7 @@ use alloc::vec::Vec;
 use core::{
     iter::Skip,
     marker::PhantomData,
+    mem::ManuallyDrop,
     sync::atomic::{AtomicUsize, Ordering},
 };
 
@@ -16,7 +17,9 @@ where
     T: Send + Sync,
     E: Enumeration,
 {
-    vec: Vec<T>,
+    ptr: *mut T,
+    vec_len: usize,
+    vec_cap: usize,
     counter: AtomicUsize,
     phantom: PhantomData<E>,
 }
@@ -27,12 +30,45 @@ where
     E: Enumeration,
 {
     fn default() -> Self {
+        Self::new(Vec::new())
+    }
+}
+
+impl<T, E> ConIterVec<T, E>
+where
+    T: Send + Sync,
+    E: Enumeration,
+{
+    pub(crate) fn new(mut vec: Vec<T>) -> Self {
+        let (vec_len, vec_cap) = (vec.len(), vec.capacity());
+        let ptr = vec.as_mut_ptr();
+        let _ = ManuallyDrop::new(vec);
         Self {
-            vec: Default::default(),
+            ptr,
+            vec_len,
+            vec_cap,
             counter: 0.into(),
             phantom: PhantomData,
         }
     }
+
+    fn progress_and_get_begin_idx(&self, number_to_fetch: usize) -> Option<usize> {
+        let begin_idx = self.counter.fetch_add(number_to_fetch, Ordering::Relaxed);
+        match begin_idx < self.vec_len {
+            true => Some(begin_idx),
+            _ => None,
+        }
+    }
+
+    // pub(super) fn progress_and_get_chunk(&self, chunk_size: usize) -> Option<(usize, &'a [T])> {
+    //     self.progress_and_get_begin_idx(chunk_size)
+    //         .map(|begin_idx| {
+    //             let end_idx = (begin_idx + chunk_size)
+    //                 .min(self.slice.len())
+    //                 .max(begin_idx);
+    //             (begin_idx, &self.slice[begin_idx..end_idx])
+    //         })
+    // }
 }
 
 impl<T, E> ConcurrentIter<E> for ConIterVec<T, E>
