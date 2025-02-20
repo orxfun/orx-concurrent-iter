ca55479bef2e536ec2ff8c263370cc506ad980df
orxfun <orx.ugur.arikan@gmail.com>
2025-02-21 00:43:53 +0100

    ChunksIterXOfIter  is implemented

--- src/implementations/iter_x/chunks_iter_x_of_iter.rs
+++ src/implementations/iter_x/chunks_iter_x_of_iter.rs
@@ -2,9 +2,10 @@ use super::con_iter_x_of_iter::ConIterXOfIter;
 use crate::chunk_puller::ChunkPuller;
 use crate::enumeration::{Element, Enumeration, Regular};
 use alloc::vec::Vec;
+use core::iter::FusedIterator;
 use core::ops::{Add, Range};
 
-pub struct ChunksIterXOfIter<'i, I, T>
+pub struct ChunksPullerXOfIter<'i, I, T>
 where
     T: Send + Sync,
     I: Iterator<Item = T>,
@@ -13,7 +14,7 @@ where
     buffer: Vec<Option<T>>,
 }
 
-impl<'i, I, T> ChunksIterXOfIter<'i, I, T>
+impl<'i, I, T> ChunksPullerXOfIter<'i, I, T>
 where
     T: Send + Sync,
     I: Iterator<Item = T>,
@@ -26,16 +27,78 @@ where
     }
 }
 
-// impl<'i, I, T> ChunkPuller<Regular> for ChunksIterXOfIter<'i, I, T>
-// where
-//     T: Send + Sync,
-//     I: Iterator<Item = T>,
-// {
-//     type ChunkItem = T;
+impl<'i, I, T> ChunkPuller<Regular> for ChunksPullerXOfIter<'i, I, T>
+where
+    T: Send + Sync,
+    I: Iterator<Item = T>,
+{
+    type ChunkItem = T;
 
-//     type Iter;
+    type Iter = ChunksIterXOfIter<'i, T>;
 
-//     fn chunk_size(&self) -> usize {
-//         todo!()
-//     }
-// }
+    fn chunk_size(&self) -> usize {
+        self.buffer.capacity()
+    }
+}
+
+impl<'i, I, T> Iterator for ChunksPullerXOfIter<'i, I, T>
+where
+    T: Send + Sync,
+    I: Iterator<Item = T>,
+{
+    type Item = ChunksIterXOfIter<'i, T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
+}
+
+pub struct ChunksIterXOfIter<'i, T>
+where
+    T: Send + Sync,
+{
+    buffer: &'i mut [Option<T>],
+    current: usize,
+}
+
+impl<'i, T> Default for ChunksIterXOfIter<'i, T>
+where
+    T: Send + Sync,
+{
+    fn default() -> Self {
+        Self {
+            buffer: &mut [],
+            current: 0,
+        }
+    }
+}
+
+impl<'i, T> Iterator for ChunksIterXOfIter<'i, T>
+where
+    T: Send + Sync,
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.buffer.get_mut(self.current).and_then(|x| {
+            self.current += 1;
+            x.take()
+        })
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.buffer.len().saturating_sub(self.current);
+        (len, Some(len))
+    }
+}
+
+impl<'i, T> ExactSizeIterator for ChunksIterXOfIter<'i, T>
+where
+    T: Send + Sync,
+{
+    fn len(&self) -> usize {
+        self.buffer.len().saturating_sub(self.current)
+    }
+}
+
+impl<'i, T> FusedIterator for ChunksIterXOfIter<'i, T> where T: Send + Sync {}
