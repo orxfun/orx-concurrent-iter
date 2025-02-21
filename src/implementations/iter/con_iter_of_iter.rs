7e3cb70deae12509ba825d2e835f763b6e8591e1
orxfun <orx.ugur.arikan@gmail.com>
2025-02-21 16:11:24 +0100

    ConcurrentIter set up

--- src/implementations/iter/con_iter_of_iter.rs
+++ src/implementations/iter/con_iter_of_iter.rs
@@ -4,41 +4,45 @@ use super::{
     num_taken::NumTaken,
 };
 use crate::{
-    chunk_puller::DoNothingChunkPuller,
+    chunk_puller::{ChunkPuller, DoNothingChunkPuller},
     concurrent_iter::ConcurrentIter,
     enumeration::{Element, Enumeration, Regular},
 };
-use core::{cell::UnsafeCell, sync::atomic::Ordering};
+use core::{cell::UnsafeCell, marker::PhantomData, sync::atomic::Ordering};
 
-pub struct ConIterOfIter<I, T>
+pub struct ConIterOfIter<I, T, E = Regular>
 where
     T: Send + Sync,
     I: Iterator<Item = T>,
+    E: Enumeration,
 {
     iter: MutIter<T, I>,
     num_taken: NumTaken,
     initial_len: Option<usize>,
     state: AtomicState,
+    phantom: PhantomData<E>,
 }
 
-unsafe impl<I: Iterator<Item = T>, T: Send + Sync> Sync for ConIterOfIter<I, T> {}
+unsafe impl<I: Iterator<Item = T>, T: Send + Sync, E: Enumeration> Sync for ConIterOfIter<I, T, E> {}
 
-unsafe impl<I: Iterator<Item = T>, T: Send + Sync> Send for ConIterOfIter<I, T> {}
+unsafe impl<I: Iterator<Item = T>, T: Send + Sync, E: Enumeration> Send for ConIterOfIter<I, T, E> {}
 
-impl<I, T> Default for ConIterOfIter<I, T>
+impl<I, T, E> Default for ConIterOfIter<I, T, E>
 where
     T: Send + Sync,
     I: Iterator<Item = T> + Default,
+    E: Enumeration,
 {
     fn default() -> Self {
         Self::new(I::default())
     }
 }
 
-impl<I, T> ConIterOfIter<I, T>
+impl<I, T, E> ConIterOfIter<I, T, E>
 where
     T: Send + Sync,
     I: Iterator<Item = T>,
+    E: Enumeration,
 {
     pub(super) fn new(iter: I) -> Self {
         let initial_len = match iter.size_hint() {
@@ -51,6 +55,39 @@ where
             initial_len,
             num_taken: 0.into(),
             state: 0.into(),
+            phantom: PhantomData,
         }
+    }
+}
+
+impl<I, T, E> ConcurrentIter<E> for ConIterOfIter<I, T, E>
+where
+    T: Send + Sync,
+    I: Iterator<Item = T>,
+    E: Enumeration,
+{
+    type Item = T;
+
+    type SeqIter = core::iter::Empty<T>;
+
+    type ChunkPuller<'i>
+        = DoNothingChunkPuller<E, T>
+    where
+        Self: 'i;
+
+    fn into_seq_iter(self) -> Self::SeqIter {
+        todo!()
+    }
+
+    fn skip_to_end(&self) {
+        todo!()
+    }
+
+    fn next(&self) -> Option<<<E as Enumeration>::Element as Element>::ElemOf<Self::Item>> {
+        todo!()
+    }
+
+    fn chunks_iter(&self, chunk_size: usize) -> Self::ChunkPuller<'_> {
+        todo!()
     }
 }
