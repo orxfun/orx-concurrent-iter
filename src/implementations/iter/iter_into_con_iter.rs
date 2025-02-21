773230c236bc705103401e612aa0f65be77163fe
orxfun <orx.ugur.arikan@gmail.com>
2025-02-21 17:38:55 +0100

    ConcurrentIterable is implemented for CloningIterable

--- src/implementations/iter/iter_into_con_iter.rs
+++ src/implementations/iter/iter_into_con_iter.rs
@@ -1,4 +1,6 @@
 use super::con_iter_of_iter::ConIterOfIter;
+use crate::ConcurrentIterable;
+use orx_iterable::{transformations::CloningIterable, Iterable};
 
 pub trait IterIntoConcurrentIter<T>: Iterator<Item = T> + Sized
 where
@@ -14,5 +16,19 @@ where
 {
     fn iter_into_concurrent_iter(self) -> ConIterOfIter<Self, T> {
         ConIterOfIter::new(self)
+    }
+}
+
+impl<I> ConcurrentIterable for CloningIterable<I>
+where
+    I: Iterator + Clone,
+    I::Item: Send + Sync,
+{
+    type Item = I::Item;
+
+    type Iter = ConIterOfIter<I, I::Item>;
+
+    fn concurrent_iter(&self) -> Self::Iter {
+        ConIterOfIter::new(self.iter())
     }
 }
