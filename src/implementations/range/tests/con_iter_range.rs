d0cf5535c6987dc061085194be8577218a52ec2f
orxfun <orx.ugur.arikan@gmail.com>
2025-02-27 10:12:11 +0100

    item_puller tests added for range

--- src/implementations/range/tests/con_iter_range.rs
+++ src/implementations/range/tests/con_iter_range.rs
@@ -151,11 +151,42 @@ where
 }
 
 #[test_matrix([Regular, Enumerated], [0, 1, N], [1, 2, 4])]
-fn chunks_iter<K: Enumeration>(_: K, n: usize, nt: usize)
+fn item_puller<E: Enumeration>(_: E, n: usize, nt: usize)
 where
-    for<'a> <K::Element as Element>::ElemOf<usize>: PartialEq + Ord + Debug,
+    for<'a> <E::Element as Element>::ElemOf<usize>: PartialEq + Ord + Debug,
 {
-    let iter = ConIterRange::<usize, K>::new(10..(10 + n));
+    let iter = ConIterRange::<usize, E>::new(10..(10 + n));
+
+    let bag = ConcurrentBag::new();
+    let num_spawned = ConcurrentBag::new();
+    std::thread::scope(|s| {
+        for _ in 0..nt {
+            s.spawn(|| {
+                num_spawned.push(true);
+                while num_spawned.len() < nt {} // allow all threads to be spawned
+
+                for x in iter.item_puller() {
+                    _ = iter.size_hint();
+                    bag.push(x);
+                }
+            });
+        }
+    });
+
+    let mut expected: Vec<_> = (0..n).map(|i| E::new_element(i, i + 10)).collect();
+    expected.sort();
+    let mut collected = bag.into_inner().to_vec();
+    collected.sort();
+
+    assert_eq!(expected, collected);
+}
+
+#[test_matrix([Regular, Enumerated], [0, 1, N], [1, 2, 4])]
+fn chunk_iter<E: Enumeration>(_: E, n: usize, nt: usize)
+where
+    for<'a> <E::Element as Element>::ElemOf<usize>: PartialEq + Ord + Debug,
+{
+    let iter = ConIterRange::<usize, E>::new(10..(10 + n));
 
     let bag = ConcurrentBag::new();
     let num_spawned = ConcurrentBag::new();
@@ -167,11 +198,11 @@ where
 
                 let mut chunks_iter = iter.chunk_puller(7);
 
-                while let Some((begin_idx, chunk)) = chunks_iter.pull().map(K::destruct_chunk) {
+                while let Some((begin_idx, chunk)) = chunks_iter.pull().map(E::destruct_chunk) {
                     assert!(chunk.len() <= 7);
                     for x in chunk {
                         _ = iter.size_hint();
-                        let value = K::new_element_from_begin_idx(begin_idx, x);
+                        let value = E::new_element_from_begin_idx(begin_idx, x);
                         bag.push(value);
                     }
                 }
@@ -182,7 +213,7 @@ where
     let mut expected = vec![];
     for i in 0..n {
         let c = (i / 7) * 7;
-        expected.push(K::new_element(c, i + 10));
+        expected.push(E::new_element(c, i + 10));
     }
     expected.sort();
     let mut collected = bag.into_inner().to_vec();
