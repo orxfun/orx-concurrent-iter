179fc1d63fa233803c4f1e154125980d513fad7b
Ugur-Arikan <uurfun@gmail.com>
2025-03-04 11:38:31 +0100

    IntoConcurrentIter for ConIterRange<T>

--- src/implementations/range/into_con_iter.rs
+++ src/implementations/range/into_con_iter.rs
@@ -15,3 +15,17 @@ where
         Self::IntoIter::new(self)
     }
 }
+
+impl<T> IntoConcurrentIter for ConIterRange<T>
+where
+    T: Send + Sync + From<usize> + Into<usize>,
+    Range<T>: Default + Clone + ExactSizeIterator<Item = T>,
+{
+    type Item = T;
+
+    type IntoIter = Self;
+
+    fn into_con_iter(self) -> Self::IntoIter {
+        self
+    }
+}
