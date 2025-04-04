b0d3da032ecaa231f3b0edc6f1acda2ab6daaff4
orxfun <orx.ugur.arikan@gmail.com>
2025-04-04 19:50:44 +0200

    is_empty is added to exact size concurrent iter

--- src/exact_size_concurrent_iter.rs
+++ src/exact_size_concurrent_iter.rs
@@ -2,4 +2,8 @@ use crate::concurrent_iter::ConcurrentIter;
 
 pub trait ExactSizeConcurrentIter: ConcurrentIter {
     fn len(&self) -> usize;
+
+    fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
 }
