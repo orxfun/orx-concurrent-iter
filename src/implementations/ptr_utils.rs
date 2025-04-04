d259f45f65e7b8c99ea93f5ccb53650c5795189f
orxfun <orx.ugur.arikan@gmail.com>
2025-04-04 19:50:07 +0200

    clippy unsafe scope fixes

--- src/implementations/ptr_utils.rs
+++ src/implementations/ptr_utils.rs
@@ -2,6 +2,6 @@ use core::mem::MaybeUninit;
 
 pub(crate) unsafe fn take<T>(ptr: *mut T) -> T {
     let mut value = MaybeUninit::<T>::uninit();
-    value.as_mut_ptr().swap(ptr);
-    value.assume_init()
+    unsafe { value.as_mut_ptr().swap(ptr) };
+    unsafe { value.assume_init() }
 }
