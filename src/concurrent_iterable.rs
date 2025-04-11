34d7a589f916c7c7c917ca8121c02574374cdfa2
orxfun <orx.ugur.arikan@gmail.com>
2025-04-11 17:28:38 +0200

    ConcurrentIterable is documented

--- src/concurrent_iterable.rs
+++ src/concurrent_iterable.rs
@@ -1,10 +1,86 @@
 use crate::{concurrent_iter::ConcurrentIter, into_concurrent_iter::IntoConcurrentIter};
 
+/// [`ConcurrentIterable`] types are those from which concurrent iterators can be created
+/// **multiple times** using the [`con_iter`] method, since this method call does not consume the source.
+///
+/// This trait can be considered as the *concurrent counterpart* of the [`Iterable`] trait.
+///
+/// [`con_iter`]: crate::ConcurrentIterable::con_iter
+/// [`Iterable`]: orx_iterable::Iterable
+///
+/// # Examples
+///
+/// ```rust
+/// use orx_concurrent_iter::*;
+///
+/// let vec = vec![1, 2]; // Vec<T>: ConcurrentIterable<Item = &T>
+/// for _ in 0..5 {
+///     let con_iter = vec.con_iter();
+///     assert_eq!(con_iter.next(), Some(&1));
+///     assert_eq!(con_iter.next(), Some(&2));
+///     assert_eq!(con_iter.next(), None);
+/// }
+///
+/// let slice = vec.as_slice(); // &[T]: ConcurrentIterable<Item = &T>
+/// for _ in 0..5 {
+///     let con_iter = slice.con_iter();
+///     assert_eq!(con_iter.next(), Some(&1));
+///     assert_eq!(con_iter.next(), Some(&2));
+///     assert_eq!(con_iter.next(), None);
+/// }
+///
+/// let range = 11..13; // Range<T>: ConcurrentIterable<Item = T>
+/// for _ in 0..5 {
+///     let con_iter = range.con_iter();
+///     assert_eq!(con_iter.next(), Some(11));
+///     assert_eq!(con_iter.next(), Some(12));
+///     assert_eq!(con_iter.next(), None);
+/// }
+/// ```
 pub trait ConcurrentIterable {
+    /// Type of the element that the concurrent iterator yields.
     type Item: Send + Sync;
 
+    /// Type of the concurrent iterator that this type can be converted into.
     type Iter: ConcurrentIter<Item = Self::Item>;
 
+    /// [`ConcurrentIterable`] types are those from which concurrent iterators can be created
+    /// **multiple times** using the [`con_iter`] method, since this method call does not consume the source.
+    ///
+    /// This trait can be considered as the *concurrent counterpart* of the [`Iterable`] trait.
+    ///
+    /// [`con_iter`]: crate::ConcurrentIterable::con_iter
+    /// [`Iterable`]: orx_iterable::Iterable
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use orx_concurrent_iter::*;
+    ///
+    /// let vec = vec![1, 2]; // Vec<T>: ConcurrentIterable<Item = &T>
+    /// for _ in 0..5 {
+    ///     let con_iter = vec.con_iter();
+    ///     assert_eq!(con_iter.next(), Some(&1));
+    ///     assert_eq!(con_iter.next(), Some(&2));
+    ///     assert_eq!(con_iter.next(), None);
+    /// }
+    ///
+    /// let slice = vec.as_slice(); // &[T]: ConcurrentIterable<Item = &T>
+    /// for _ in 0..5 {
+    ///     let con_iter = slice.con_iter();
+    ///     assert_eq!(con_iter.next(), Some(&1));
+    ///     assert_eq!(con_iter.next(), Some(&2));
+    ///     assert_eq!(con_iter.next(), None);
+    /// }
+    ///
+    /// let range = 11..13; // Range<T>: ConcurrentIterable<Item = T>
+    /// for _ in 0..5 {
+    ///     let con_iter = range.con_iter();
+    ///     assert_eq!(con_iter.next(), Some(11));
+    ///     assert_eq!(con_iter.next(), Some(12));
+    ///     assert_eq!(con_iter.next(), None);
+    /// }
+    /// ```
     fn con_iter(&self) -> Self::Iter;
 }
 
