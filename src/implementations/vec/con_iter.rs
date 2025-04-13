3e47b1531dbff8789834d07edad76feea37e6d59
orxfun <orx.ugur.arikan@gmail.com>
2025-04-13 20:18:06 +0200

    ConIterVec documentation is added

--- src/implementations/vec/con_iter.rs
+++ src/implementations/vec/con_iter.rs
@@ -9,6 +9,23 @@ use core::{
     sync::atomic::{AtomicUsize, Ordering},
 };
 
+/// Concurrent iterator of a [`Vec`].
+///
+/// It can be created by calling [`into_con_iter`] on a vector.
+///
+/// [`into_con_iter`]: crate::IntoConcurrentIter::into_con_iter
+///
+/// # Examples
+///
+/// ```
+// /use orx_concurrent_iter::*;
+// /
+// /let vec = vec![1, 2];
+// /let con_iter = vec.into_con_iter();
+// /assert_eq!(con_iter.next(), Some(1));
+// /assert_eq!(con_iter.next(), Some(2));
+// /assert_eq!(con_iter.next(), None);
+/// ```
 pub struct ConIterVec<T>
 where
     T: Send + Sync,
