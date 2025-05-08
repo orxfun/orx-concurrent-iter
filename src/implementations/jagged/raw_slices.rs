9e88916b6a80b211c348f3745687ad1bf44799e9
orxfun <orx.ugur.arikan@gmail.com>
2025-05-08 05:30:23 +0200

    raw slices constructor defined

--- src/implementations/jagged/raw_slices.rs
+++ src/implementations/jagged/raw_slices.rs
@@ -4,3 +4,15 @@ use alloc::vec::Vec;
 pub struct RawSlices<'a, T> {
     slices: Vec<RawSlice<'a, T>>,
 }
+
+impl<'a, T, I> From<I> for RawSlices<'a, T>
+where
+    T: 'a,
+    I: Iterator<Item = &'a [T]>,
+{
+    fn from(slices: I) -> Self {
+        Self {
+            slices: slices.map(Into::into).collect(),
+        }
+    }
+}
