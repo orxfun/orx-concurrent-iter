17e13be2b8c96523357ca6253fe3a37b8d035506
orxfun <orx.ugur.arikan@gmail.com>
2025-05-09 05:42:42 +0200

    fix RawJaggedSliceIterRef next slice implementation

--- src/implementations/jagged/mod.rs
+++ src/implementations/jagged/mod.rs
@@ -1,4 +1,6 @@
 mod con_iter;
 mod raw_jagged;
 mod raw_jagged_slice;
+mod raw_jagged_slice_iter_owned;
+mod raw_jagged_slice_iter_ref;
 mod raw_slice;
--- src/implementations/jagged/raw_jagged_slice_iter_ref.rs
+++ src/implementations/jagged/raw_jagged_slice_iter_ref.rs
@@ -1,4 +1,4 @@
-use super::{raw_jagged_slice::RawJaggedSlice, raw_slice::RawSlice};
+use super::raw_jagged_slice::RawJaggedSlice;
 
 pub struct RawJaggedSliceIterRef<'a, T> {
     slice: RawJaggedSlice<'a, T>,
@@ -9,7 +9,8 @@ pub struct RawJaggedSliceIterRef<'a, T> {
 impl<'a, T> RawJaggedSliceIterRef<'a, T> {
     pub(super) fn new(slice: RawJaggedSlice<'a, T>) -> Self {
         let f = 0;
-        let current = slice.get_slice(f).unwrap_or(Default::default());
+        let first_slice = slice.get_slice(f).unwrap_or(Default::default());
+        let current = first_slice.iter();
         Self { slice, f, current }
     }
 
@@ -17,7 +18,8 @@ impl<'a, T> RawJaggedSliceIterRef<'a, T> {
         match self.f == self.slice.num_slices() - 1 {
             false => {
                 self.f += 1;
-                self.current = slice.get_slice(f).unwrap_or(Default::default());
+                let new_slice = self.slice.get_slice(self.f).unwrap_or(Default::default());
+                self.current = new_slice.iter();
                 self.next()
             }
             true => None,
