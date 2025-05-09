23ca011545528b6e84f5922c2a2472d85e18c1c8
orxfun <orx.ugur.arikan@gmail.com>
2025-05-09 05:37:16 +0200

    RawJaggedSliceIterRef constructor is defined

--- src/implementations/jagged/raw_jagged_slice_iter_ref.rs
+++ src/implementations/jagged/raw_jagged_slice_iter_ref.rs
@@ -2,14 +2,17 @@ use super::{raw_jagged_slice::RawJaggedSlice, raw_slice::RawSlice};
 
 pub struct RawJaggedSliceIterRef<'a, T> {
     slice: RawJaggedSlice<'a, T>,
+    f: usize,
     current: core::slice::Iter<'a, T>,
 }
 
-// impl<'a, T> RawJaggedSliceIterRef<'a, T> {
-//     pub(super) fn new(slice: RawJaggedSlice<'a, T>) -> Self {
-//         Self { slice }
-//     }
-// }
+impl<'a, T> RawJaggedSliceIterRef<'a, T> {
+    pub(super) fn new(slice: RawJaggedSlice<'a, T>) -> Self {
+        let f = 0;
+        let current = slice.get_slice(f).unwrap_or(Default::default());
+        Self { slice, f, current }
+    }
+}
 
 impl<'a, T> Iterator for RawJaggedSliceIterRef<'a, T> {
     type Item = &'a T;
