a5f78741c160a2189bfcebdd06d95c0c53f1e50a
orxfun <orx.ugur.arikan@gmail.com>
2025-05-10 22:47:01 +0200

    tests extended

--- src/implementations/jagged/tests/raw_jagged_iter_owned.rs
+++ src/implementations/jagged/tests/raw_jagged_iter_owned.rs
@@ -53,3 +53,73 @@ fn raw_jagged_iter_owned_matrix(consume_taken: bool, consume_remaining: bool) {
         }
     }
 }
+
+// jagged
+
+fn get_jagged() -> Vec<Vec<String>> {
+    let jagged = vec![vec![0, 1], vec![2, 3, 4], vec![5], vec![6, 7, 8, 9]];
+    jagged
+        .into_iter()
+        .map(|x| x.into_iter().map(|x| x.to_string()).collect::<Vec<_>>())
+        .collect()
+}
+
+fn jagged_indexer() -> impl Fn(usize) -> [usize; 2] {
+    let lengths = vec![2, 3, 1, 4];
+    move |idx| match idx == lengths.iter().sum::<usize>() {
+        true => [lengths.len() - 1, lengths[lengths.len() - 1]],
+        false => {
+            let mut idx = idx;
+            let [mut f, mut i] = [0, 0];
+            let mut current_f = 0;
+            while idx > 0 {
+                let current_len = lengths[current_f];
+                match current_len > idx {
+                    true => {
+                        i = idx;
+                        idx = 0;
+                    }
+                    false => {
+                        f += 1;
+                        idx -= current_len;
+                    }
+                }
+                current_f += 1;
+            }
+            [f, i]
+        }
+    }
+}
+
+#[test_matrix(
+    [true, false],
+    [true, false]
+)]
+fn raw_jagged_iter_owned_jagged(consume_taken: bool, consume_remaining: bool) {
+    let len = 10;
+
+    let jagged = || {
+        let jagged = get_jagged();
+        let vectors: Vec<_> = jagged.into_iter().map(RawVec::from).collect();
+        RawJagged::new(vectors.into_iter(), jagged_indexer(), true)
+    };
+
+    for num_taken in 0..len {
+        let mut jagged = jagged();
+        jagged.set_num_taken(num_taken);
+
+        {
+            let iter_owned = jagged.slice(0, num_taken).into_iter_owned();
+            if consume_taken {
+                let _from_jagged: Vec<_> = iter_owned.collect();
+            }
+        }
+
+        let remaining = jagged.into_iter_owned();
+        if consume_remaining {
+            let remaining: Vec<_> = remaining.collect();
+            let expected_remaining: Vec<_> = (num_taken..len).map(|x| x.to_string()).collect();
+            assert_eq!(remaining, expected_remaining);
+        }
+    }
+}
