5d5bc95d4668b640c23f0246d3148da28e66dd16
orxfun <orx.ugur.arikan@gmail.com>
2025-05-16 19:35:16 +0200

    clean up

--- src/implementations/jagged_arrays/raw_slice.rs
+++ src/implementations/jagged_arrays/raw_slice.rs
@@ -21,21 +21,6 @@ impl<T> RawSlice<T> {
     pub(super) fn new(ptr: *const T, len: usize) -> Self {
         Self { ptr, len }
     }
-
-    /// # SAFETY
-    ///
-    /// The caller must ensure that the slice does not overlive the data source jagged array.
-    pub(super) unsafe fn as_slice<'b>(self) -> &'b [T] {
-        unsafe { core::slice::from_raw_parts(self.ptr, self.len) }
-    }
-
-    /// # SAFETY
-    ///
-    /// The caller must ensure that the reference does not outlive the data source jagged array.
-    pub(super) unsafe fn get_unchecked<'b>(&self, i: usize) -> &'b T {
-        debug_assert!(i < self.len);
-        unsafe { &*self.ptr.add(i) }
-    }
 }
 
 impl<T> AsRawSlice<T> for RawSlice<T> {
