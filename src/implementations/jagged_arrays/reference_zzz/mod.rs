4384a3de663925fa58201105035c251a1a85bf8d
orxfun <orx.ugur.arikan@gmail.com>
2025-05-16 19:33:43 +0200

    clean up

--- src/implementations/jagged_arrays/reference_zzz/mod.rs
+++
@@ -1,6 +0,0 @@
-mod chunk_puller;
-mod con_iter;
-mod raw_jagged_ref;
-mod slice_iter;
-
-// pub use raw_jagged_ref::RawJaggedRef;
--- src/implementations/jagged_arrays/reference_zzzzz/mod.rs
+++
@@ -1,5 +0,0 @@
-// mod as_raw_jagged_ref;
-// mod chunk_puller;
-// mod con_iter;
-// mod slice;
-// mod slice_iter;
