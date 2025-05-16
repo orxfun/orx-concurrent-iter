697d40e8b5d2e5aaaa09c1d03725ce3332312dbb
orxfun <orx.ugur.arikan@gmail.com>
2025-05-16 23:43:17 +0200

    IntoConcurrentIter for VecDeque<T>

--- src/implementations/std_implementations/vec_deque/into_con_iter.rs
+++ src/implementations/std_implementations/vec_deque/into_con_iter.rs
@@ -1,6 +1,6 @@
 use super::con_iter_ref::ConIterVecDequeRef;
-use crate::IntoConcurrentIter;
-use std::collections::VecDeque;
+use crate::{IntoConcurrentIter, implementations::ConIterVec};
+use std::{collections::VecDeque, vec::Vec};
 
 impl<'a, T> IntoConcurrentIter for &'a VecDeque<T>
 where
@@ -12,5 +12,18 @@ where
 
     fn into_con_iter(self) -> Self::IntoIter {
         Self::IntoIter::new(self)
+    }
+}
+
+impl<T> IntoConcurrentIter for VecDeque<T>
+where
+    T: Send + Sync,
+{
+    type Item = T;
+
+    type IntoIter = ConIterVec<T>;
+
+    fn into_con_iter(self) -> Self::IntoIter {
+        Vec::from(self).into_con_iter()
     }
 }
