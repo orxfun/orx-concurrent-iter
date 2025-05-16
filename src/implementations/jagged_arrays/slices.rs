28305cf80040ba29b7083495145fde9829ddcdc9
orxfun <orx.ugur.arikan@gmail.com>
2025-05-16 23:44:57 +0200

    slices documentation

--- src/implementations/jagged_arrays/slices.rs
+++ src/implementations/jagged_arrays/slices.rs
@@ -1,19 +1,31 @@
 use alloc::vec::Vec;
 
+/// A collection of slices.
 pub trait Slices<'a, T>: Clone
 where
     T: 'a,
 {
+    /// Creates an empty collection of slices.
     fn empty() -> Self;
 
+    /// Number of slices.
     fn num_slices(&self) -> usize;
 
+    /// Iterator over slices.
     fn slices(&self) -> impl Iterator<Item = &'a [T]>;
 
+    /// Iterator over lengths of slices.
     fn lengths(&self) -> impl Iterator<Item = usize>;
 
+    /// Returns a reference to the `f`-th slice.
+    /// Returns None if out of bounds.
     fn slice_at(&self, f: usize) -> Option<&'a [T]>;
 
+    /// Returns the `f`-th slice without bounds checks.
+    ///
+    /// # Safety
+    ///
+    /// The caller must ensure that `f < self.num_slices()`.
     unsafe fn slice_at_unchecked(&self, f: usize) -> &'a [T];
 }
 
