d0004e339a9509cc8c1314aca7807bfd20acd70d
orxfun <orx.ugur.arikan@gmail.com>
2025-08-05 09:18:29 +0200

    IntoConcurrentIter for &'a mut Vec<T> and hence ConcurrentCollectionMut for Vec

--- src/implementations/vec/into_con_iter.rs
+++ src/implementations/vec/into_con_iter.rs
@@ -1,5 +1,8 @@
 use super::con_iter::ConIterVec;
-use crate::{implementations::slice::ConIterSlice, into_concurrent_iter::IntoConcurrentIter};
+use crate::{
+    implementations::{slice::ConIterSlice, slice_mut::ConIterSliceMut},
+    into_concurrent_iter::IntoConcurrentIter,
+};
 use alloc::vec::Vec;
 
 impl<T> IntoConcurrentIter for Vec<T>
@@ -25,5 +28,18 @@ where
 
     fn into_con_iter(self) -> Self::IntoIter {
         Self::IntoIter::new(self.as_slice())
+    }
+}
+
+impl<'a, T> IntoConcurrentIter for &'a mut Vec<T>
+where
+    T: Sync,
+{
+    type Item = &'a mut T;
+
+    type IntoIter = ConIterSliceMut<'a, T>;
+
+    fn into_con_iter(self) -> Self::IntoIter {
+        Self::IntoIter::new(self.as_mut_slice())
     }
 }
