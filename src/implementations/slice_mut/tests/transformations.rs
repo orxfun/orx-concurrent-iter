adcbe3e2115adad8ae652b73ecc55a87d0f27986
orxfun <orx.ugur.arikan@gmail.com>
2025-08-05 07:13:22 +0200

    transformations tests for ConIterSliceMut

--- src/implementations/slice_mut/tests/transformations.rs
+++ src/implementations/slice_mut/tests/transformations.rs
@@ -1,77 +1,62 @@
-use crate::{ChunkPuller, ConcurrentIter, concurrent_iterable::ConcurrentIterable};
+use crate::{ChunkPuller, ConcurrentIter, IntoConcurrentIter};
 use alloc::vec::Vec;
 
-// #[test]
-// fn enumerate_item() {
-//     let mut vec: Vec<_> = (2..6).collect();
-//     let slice = vec.as_mut_slice();
-//     let iter = slice.con_iter().enumerate();
-//     let mut j = 0;
-//     while let Some((i, x)) = iter.next() {
-//         assert_eq!((i, x), (j, &(j + 2)));
-//         j += 1;
-//     }
+#[test]
+fn enumerate_item() {
+    let mut vec: Vec<_> = (2..6).collect();
+    let slice = vec.as_mut_slice();
+    let iter = slice.into_con_iter().enumerate();
+    let mut j = 0;
+    while let Some((i, x)) = iter.next() {
+        assert_eq!((i, x), (j, &mut (j + 2)));
+        j += 1;
+    }
 
-//     let mut vec: Vec<_> = (2..6).collect();
-//     let slice = vec.as_mut_slice();
-//     let iter = slice.con_iter().enumerate();
-//     let mut j = 0;
-//     while let Some((i2, (i, x))) = iter.next_with_idx() {
-//         assert_eq!((i, x), (j, &(j + 2)));
-//         assert_eq!(i, i2);
-//         j += 1;
-//     }
-// }
+    let mut vec: Vec<_> = (2..6).collect();
+    let slice = vec.as_mut_slice();
+    let iter = slice.into_con_iter().enumerate();
+    let mut j = 0;
+    while let Some((i2, (i, x))) = iter.next_with_idx() {
+        assert_eq!((i, x), (j, &mut (j + 2)));
+        assert_eq!(i, i2);
+        j += 1;
+    }
+}
 
-// #[test]
-// fn enumerate_item_puller() {
-//     let mut vec: Vec<_> = (2..6).collect();
-//     let slice = vec.as_mut_slice();
-//     let iter = slice.con_iter().enumerate();
-//     let puller = iter.item_puller();
-//     let collected: Vec<_> = puller.collect();
-//     assert_eq!(collected, [0, 1, 2, 3].map(|x| (x, &vec[x])));
+#[test]
+fn enumerate_item_puller() {
+    let mut vec: Vec<_> = (2..6).collect();
+    let slice = vec.as_mut_slice();
+    let iter = slice.into_con_iter().enumerate();
+    let puller = iter.item_puller();
+    for (i, x) in puller {
+        assert_eq!(x.clone(), 2 + i);
+    }
 
-//     let mut vec: Vec<_> = (2..6).collect();
-//     let slice = vec.as_mut_slice();
-//     let iter = slice.con_iter().enumerate();
-//     let puller = iter.item_puller_with_idx();
-//     let collected: Vec<_> = puller.collect();
-//     assert_eq!(collected, [0, 1, 2, 3].map(|x| (x, (x, &vec[x]))));
-// }
+    let mut vec: Vec<_> = (2..6).collect();
+    let slice = vec.as_mut_slice();
+    let iter = slice.into_con_iter().enumerate();
+    let puller = iter.item_puller_with_idx();
+    for (i, (j, x)) in puller {
+        assert_eq!(i, j);
+        assert_eq!(x.clone(), 2 + i);
+    }
+}
 
-// #[test]
-// fn enumerate_chunk_puller() {
-//     let mut vec: Vec<_> = (2..6).collect();
-//     let slice = vec.as_mut_slice();
-//     let iter = slice.con_iter().enumerate();
+#[test]
+fn enumerate_chunk_puller() {
+    let mut vec: Vec<_> = (2..6).collect();
+    let slice = vec.as_mut_slice();
+    let iter = slice.into_con_iter().enumerate();
 
-//     let mut j = 0;
+    let mut j = 0;
 
-//     let mut puller = iter.chunk_puller(2);
-//     while let Some(chunk) = puller.pull() {
-//         assert_eq!(chunk.len(), 2);
-//         for (i, x) in chunk {
-//             assert_eq!((i, x), (j, &(j + 2)));
-//             j += 1;
-//         }
-//     }
-// }
-
-// #[test]
-// fn copied() {
-//     let mut vec: Vec<_> = (2..6).collect();
-//     let slice = vec.as_mut_slice();
-//     let iter = slice.con_iter().copied();
-//     let values: Vec<_> = iter.item_puller().collect();
-//     assert_eq!(values, vec);
-// }
-
-// #[test]
-// fn cloned() {
-//     let mut vec: Vec<_> = (2..6).collect();
-//     let slice = vec.as_mut_slice();
-//     let iter = slice.con_iter().cloned();
-//     let values: Vec<_> = iter.item_puller().collect();
-//     assert_eq!(values, vec);
-// }
+    let mut puller = iter.chunk_puller(2);
+    while let Some(chunk) = puller.pull() {
+        assert_eq!(chunk.len(), 2);
+        for (i, x) in chunk {
+            assert_eq!((i, x), (j, &mut (j + 2)));
+            j += 1;
+        }
+    }
+}
