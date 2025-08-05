a13a451976de3c6bab2c0fc12e902cd0635fffc1
orxfun <orx.ugur.arikan@gmail.com>
2025-08-05 09:35:03 +0200

    ConcurrentCollectionMut documentation

--- src/concurrent_collection_mut.rs
+++ src/concurrent_collection_mut.rs
@@ -1,5 +1,37 @@
 use crate::{ConcurrentCollection, ConcurrentIter, IntoConcurrentIter};
 
+/// A type implementing [`ConcurrentCollectionMut`] is a collection owning the elements such that
+///
+/// * if the elements are of type `T`,
+/// * then, non-consuming [`con_iter`] method can be called **multiple times** to create concurrent
+///   iterators; i.e., [`ConcurrentIter`], yielding references to the elements `&T`; and further,
+/// * non-consuming mutable [`con_iter_mut`] method can be called to create concurrent iterators
+///   yielding mutable references to elements `&mut T`.
+///
+/// This trait can be considered as the *concurrent counterpart* of the [`CollectionMut`] trait.
+///
+/// [`con_iter`]: crate::ConcurrentCollection::con_iter
+/// [`CollectionMut`]: orx_iterable::CollectionMut
+/// [`ConcurrentIter`]: crate::ConcurrentIter
+///
+/// # Examples
+///
+/// ```
+/// use orx_concurrent_iter::*;
+///
+/// let mut data = vec![1, 2];
+///
+/// let con_iter = data.con_iter_mut();
+/// assert_eq!(con_iter.next(), Some(&mut 1));
+/// assert_eq!(con_iter.next(), Some(&mut 2));
+/// assert_eq!(con_iter.next(), None);
+///
+/// let con_iter = data.con_iter_mut();
+/// while let Some(x) = con_iter.next() {
+///     *x *= 100;
+/// }
+/// assert_eq!(vec, vec![100, 200]);
+/// ```
 pub trait ConcurrentCollectionMut: ConcurrentCollection {
     type IterMut<'a>: ConcurrentIter<Item = &'a mut Self::Item>
     where
