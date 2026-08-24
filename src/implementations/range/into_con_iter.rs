d033e6a1200946a4df0b6d6c798d8e5baafb73ee
orxfun <orx.ugur.arikan@gmail.com>
2026-08-24 16:09:52 +0200

    implement into concurrent iter for inclusive ranges

--- src/implementations/range/into_con_iter.rs
+++ src/implementations/range/into_con_iter.rs
@@ -1,6 +1,6 @@
 use super::con_iter::ConIterRange;
 use crate::into_concurrent_iter::IntoConcurrentIter;
-use core::ops::Range;
+use core::ops::{Range, RangeInclusive};
 
 impl<T> IntoConcurrentIter for Range<T>
 where
@@ -14,4 +14,29 @@ where
     fn into_con_iter(self) -> Self::IntoIter {
         Self::IntoIter::new(self)
     }
+}
+
+impl<T> IntoConcurrentIter for RangeInclusive<T>
+where
+    T: Send + From<usize> + Into<usize> + Clone,
+    Range<T>: Default + Clone + ExactSizeIterator<Item = T>,
+{
+    type Item = T;
+
+    type IntoIter = ConIterRange<T>;
+
+    fn into_con_iter(self) -> Self::IntoIter {
+        range_inclusive_to_range(self).into_con_iter()
+    }
+}
+
+pub(super) fn range_inclusive_to_range<T>(range_inclusive: RangeInclusive<T>) -> Range<T>
+where
+    T: Send + From<usize> + Into<usize> + Clone,
+    Range<T>: Default + Clone + ExactSizeIterator<Item = T>,
+{
+    let a = range_inclusive.start().clone();
+    let b: usize = (range_inclusive.end().clone()).into().saturating_add(1);
+    let b: T = b.into();
+    a..b
 }
--- src/implementations/range/tests/into.rs
+++ src/implementations/range/tests/into.rs
@@ -1,6 +1,4 @@
-use crate::{
-    IntoConcurrentIter, concurrent_iter::ConcurrentIter, concurrent_iterable::ConcurrentIterable,
-};
+use crate::{ConcurrentIter, ConcurrentIterable, IntoConcurrentIter, IterIntoConcurrentIter};
 use alloc::vec::Vec;
 use orx_concurrent_bag::ConcurrentBag;
 
@@ -55,6 +53,64 @@ fn range_as_concurrent_iterable() {
     });
 
     let mut expected: Vec<_> = (0..n).map(|i| i + 10).collect();
+    expected.sort();
+    let mut collected = bag.into_inner().to_vec();
+    collected.sort();
+
+    assert_eq!(expected, collected);
+}
+
+#[test]
+fn range_inclusive_as_into_concurrent_iter() {
+    let (nt, n) = (2, 177);
+    let range = 10..=(10 + n);
+    let iter = range.into_con_iter();
+
+    let bag = ConcurrentBag::new();
+    let num_spawned = ConcurrentBag::new();
+    std::thread::scope(|s| {
+        for _ in 0..nt {
+            s.spawn(|| {
+                num_spawned.push(true);
+                while num_spawned.len() < nt {} // allow all threads to be spawned
+
+                while let Some(x) = iter.next() {
+                    bag.push(x);
+                }
+            });
+        }
+    });
+
+    let mut expected: Vec<_> = (0..=n).map(|i| 10 + i).collect();
+    expected.sort();
+    let mut collected = bag.into_inner().to_vec();
+    collected.sort();
+
+    assert_eq!(expected, collected);
+}
+
+#[test]
+fn range_inclusive_as_concurrent_iterable() {
+    let (nt, n) = (2, 177);
+    let range = 10..=(10 + n);
+    let iter = range.con_iter();
+
+    let bag = ConcurrentBag::new();
+    let num_spawned = ConcurrentBag::new();
+    std::thread::scope(|s| {
+        for _ in 0..nt {
+            s.spawn(|| {
+                num_spawned.push(true);
+                while num_spawned.len() < nt {} // allow all threads to be spawned
+
+                while let Some(x) = iter.next() {
+                    bag.push(x);
+                }
+            });
+        }
+    });
+
+    let mut expected: Vec<_> = (0..=n).map(|i| i + 10).collect();
     expected.sort();
     let mut collected = bag.into_inner().to_vec();
     collected.sort();
